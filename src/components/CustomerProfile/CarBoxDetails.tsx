b5e100d427f9e0094a7852b06c27b3f4b53cfa1d
AliHMohammad <alihmohammad@hotmail.com>
2023-12-01 19:38:16 +0100

    save

--- src/components/CustomerProfile/CarBoxDetails.tsx
+++ src/components/CustomerProfile/CarBoxDetails.tsx
@@ -1,10 +1,31 @@
 import DetailBox from './DetailBox';
+import { FaTrashAlt } from 'react-icons/fa';
+
+async function deleteCar(id: number) {
+	return fetch(`http://localhost:3000/cars/${id}`, {
+		method: "DELETE"
+	})
+}
+
 
 export default function CarBoxDetails({ car }: { car: ICar }) {
+
+	const handleDelete = async () => {
+		try {
+			const promise = await deleteCar(car.id)
+		} catch (error: any) {
+
+		}
+	}
+
+
 	return (
 		<>
 			<DetailBox title={'Model'} value={car.brand} />
 			<DetailBox title={'Registerings nr.'} value={car.registrationNumber} />
+			<div style={{cursor: "pointer"}} onClick={handleDelete}>
+				<FaTrashAlt />
+			</div>
 		</>
 	);
 }
