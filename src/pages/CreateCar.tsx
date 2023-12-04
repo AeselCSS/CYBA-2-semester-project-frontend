9edf360340863876d06e8c30e68e9171e8313ce5
AliHMohammad <alihmohammad@hotmail.com>
2023-12-04 14:11:24 +0100

    Refactored frontend

--- src/pages/CreateCar.tsx
+++ src/pages/CreateCar.tsx
@@ -2,29 +2,21 @@ import PageLayout from './PageLayout.tsx';
 import { useNavigate } from 'react-router-dom';
 import React, { useState } from 'react';
 
-interface SynsBasenAPI {
-	vin: string,
-	brand: string,
-	model: string,
-	variant: string,
-	first_registration_date: string,
-	last_inspection_date: string,
-	last_inspection_result: string,
-	last_inspection_kind: string,
-}
-
-interface INewCar {
-	customerId: string,
+interface IAPICar {
 	registrationNumber: string,
 	vinNumber: string,
 	brand: string,
 	model: string,
 	modelVariant: string,
 	firstRegistration: string,
-	mileage: number,
 	lastInspectionDate: string,
 	lastInspectionResult: string,
 	lastInspectionKind: string,
+}
+
+interface INewCar extends IAPICar {
+	customerId: string,
+	mileage: number,
 }
 
 async function createCar(newCar: INewCar) {
@@ -41,12 +33,11 @@ async function createCar(newCar: INewCar) {
 export default function CreateCar({ customer }: {
 	customer: ICustomer
 }) {
-	const [APIResult, setAPIResult] = useState<SynsBasenAPI | null>(null);
+	const [APIResult, setAPIResult] = useState<IAPICar | null>(null);
 	const [registrationNumber, setRegistrationNumber] = useState('');
 	const [mileage, setMileage] = useState('');
 	const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
 	const navigate = useNavigate();
-	const apiKey = import.meta.env.VITE_API_SYNSBASEN_TOKEN as string;
 	console.log(customer);
 	console.log(APIResult);
 
@@ -58,14 +49,14 @@ export default function CreateCar({ customer }: {
 				customerId: customer.id,
 				mileage: parseInt(mileage),
 				registrationNumber: registrationNumber,
-				vinNumber: APIResult.vin,
+				vinNumber: APIResult.vinNumber,
 				brand: APIResult.brand,
 				model: APIResult.model,
-				modelVariant: APIResult.variant,
-				firstRegistration: APIResult.first_registration_date,
-				lastInspectionDate: APIResult.last_inspection_date,
-				lastInspectionKind: APIResult.last_inspection_kind,
-				lastInspectionResult: APIResult.last_inspection_result,
+				modelVariant: APIResult.modelVariant,
+				firstRegistration: APIResult.firstRegistration,
+				lastInspectionDate: APIResult.lastInspectionDate,
+				lastInspectionKind: APIResult.lastInspectionKind,
+				lastInspectionResult: APIResult.lastInspectionResult,
 			};
 
 			console.log(newCar);
@@ -82,18 +73,16 @@ export default function CreateCar({ customer }: {
 			}
 		}
 	}
-	//https://cors-anywhere.herokuapp.com/
+
 	const handleAPIGet = async () => {
-		const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.synsbasen.dk/v1/vehicles/registration/${registrationNumber}`, {
+		const response = await fetch(`http://localhost:3000/cars/registration/${registrationNumber}`, {
 			method: 'GET',
-			headers: {
-				'Authorization': `Bearer ${apiKey}`,
-			},
 		});
 
 		if (response.ok) {
-			const { data } = await response.json();
-			setAPIResult(data);
+			const carDetails = await response.json();
+			console.log(carDetails);
+			setAPIResult(carDetails);
 			setIsSubmitDisabled(false);
 		} else {
 			setAPIResult(null);
@@ -106,68 +95,67 @@ export default function CreateCar({ customer }: {
 	};
 
 	return (
-		<>
-			<PageLayout>
-				<div>
+		<PageLayout>
+			<div>
+				<p>
+					<label htmlFor='registrationNumber'>Registerings Nr.</label>
+					<input value={registrationNumber} onChange={handleInput} id='registrationNumber' placeholder='Registerings nr.' required={true} />
+				</p>
+				<button onClick={handleAPIGet}>Søg efter oplysninger</button>
+
+				<form onSubmit={handleSubmit}>
+
 					<p>
-						<label htmlFor='registrationNumber'>Registerings Nr.</label>
-						<input value={registrationNumber} onChange={handleInput} id='registrationNumber' placeholder='Registerings nr.' required={true} />
+						<label htmlFor='mileage'>KM KØRT</label>
+						<input type='tel' pattern='[0-9]{0,6}' placeholder='6 cifre maks..' required={true} value={mileage} onChange={(e) => setMileage(e.target.value)} />
 					</p>
-					<button onClick={handleAPIGet}>Søg efter oplysninger</button>
-					
-					<form onSubmit={handleSubmit}>
 
-						<p>
-							<label htmlFor='mileage'>KM KØRT</label>
-							<input type='tel' pattern='[0-9]{0,6}' placeholder='6 cifre maks..' required={true} value={mileage} onChange={(e) => setMileage(e.target.value)} />
-						</p>
+					<p>
+						<label htmlFor='vinNumber'>STEL NR</label>
+						<input value={APIResult?.vinNumber ?? ''} disabled={true} required={true} />
+					</p>
 
-						<p>
-							<label htmlFor='vinNumber'>STEL NR</label>
-							<input value={APIResult?.vin ?? ''} disabled={true} required={true}  />
-						</p>
+					<p>
+						<label htmlFor='brand'>Mærke</label>
+						<input value={APIResult?.brand ?? ''} disabled={true} />
+					</p>
+					<p>
+						<label htmlFor='model'>Model</label>
+						<input value={APIResult?.model ?? ''} disabled={true} />
+					</p>
 
-						<p>
-							<label htmlFor='brand'>Mærke</label>
-							<input value={APIResult?.brand ?? ''} disabled={true}/>
-						</p>
-						<p>
-							<label htmlFor='model'>Model</label>
-							<input value={APIResult?.model ?? ''} disabled={true}  />
-						</p>
+					<p>
+						<label htmlFor='modelVariant'>Variant</label>
+						<input value={APIResult?.modelVariant ?? ''} disabled={true} />
+					</p>
 
-						<p>
-							<label htmlFor='modelVariant'>Variant</label>
-							<input value={APIResult?.variant ?? ''} disabled={true} />
-						</p>
+					<p>
+						<label htmlFor='firstRegistration'>Først Registreret</label>
+						<input value={APIResult?.firstRegistration ?? ''} disabled={true} type='date' />
+					</p>
 
-						<p>
-							<label htmlFor='firstRegistration'>Først Registreret</label>
-							<input value={APIResult?.first_registration_date ?? ''} disabled={true} type='date' />
-						</p>
-
-						<p>
-							<label htmlFor='lastInspectionDate'>Sidste inspektionsdato</label>
-							<input value={APIResult?.last_inspection_date ?? ''} disabled={true} type='date'  />
-						</p>
+					<p>
+						<label htmlFor='lastInspectionDate'>Sidste inspektionsdato</label>
+						<input value={APIResult?.lastInspectionDate ?? ''} disabled={true} type='date' />
+					</p>
 
 
-						<p>
-							<label htmlFor='lastInspectionResult'>Sidste inspektionsresultat</label>
-							<input value={APIResult?.last_inspection_result ?? ''} disabled={true} placeholder='' />
+					<p>
+						<label htmlFor='lastInspectionResult'>Sidste inspektionsresultat</label>
+						<input value={APIResult?.lastInspectionResult ?? ''} disabled={true} placeholder='' />
 
-						</p>
+					</p>
 
-						<p>
-							<label htmlFor='lastInspectionKind'>Sidste inspektionstype</label>
-							<input value={APIResult?.last_inspection_kind ?? ''} disabled={true} placeholder='' />
+					<p>
+						<label htmlFor='lastInspectionKind'>Sidste inspektionstype</label>
+						<input value={APIResult?.lastInspectionKind ?? ''} disabled={true} placeholder='' />
 
-						</p>
+					</p>
 
-						<input type='submit' disabled={isSubmitDisabled} />
-					</form>
-				</div>
-			</PageLayout>
-		</>
+					<input type='submit' disabled={isSubmitDisabled} />
+				</form>
+			</div>
+		</PageLayout>
+
 	);
 }
