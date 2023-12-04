c4beec54640b2b0a90274caae4fba3ce0ad3cf99
YawHB <yawhboateng@gmail.com>
2023-12-04 14:54:10 +0100

    Added basic info for customer car and order. changes may occur

--- src/components/Modal/DetailedCustomer.tsx
+++ src/components/Modal/DetailedCustomer.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
 
-export default function DetailedCustomer({ customer }: { customer: ICustomer }) {
+interface DetailedCustomerProps {
+	customer: ICustomer;
+}
+
+export default function DetailedCustomer({ customer }: DetailedCustomerProps) {
 	const [customerData, setCustomerData] = useState<IAPISingleCustomer | null>(null);
 
 	useEffect(() => {
@@ -12,7 +16,30 @@ export default function DetailedCustomer({ customer }: { customer: ICustomer }) 
 		}
 		getCustomer();
 	}, [customer.id]);
-	console.log(customerData);
+	console.log(customerData?.orders);
 
-	return <div>{customer.firstName}</div>;
+	// const filteredOrders = customerData?.orders?.filter((order) => order.status.toString() !== 'COMPLETED') || [];
+
+	return (
+		<div>
+			<p>Ordre data</p>
+			<div>{customerData?.customer.lastName}</div>
+			{customerData?.orders?.map((order) => (
+				<div key={order.id}>
+					{' '}
+					<div>{order.status}</div>
+					<div>{order.createdAt.toString()}</div>
+					<div>{order.updatedAt.toString()}</div>
+				</div>
+			))}
+			<p>Bil data</p>
+			{customerData?.cars.map((car) => (
+				<div key={car.id}>
+					<div>{car.registrationNumber}</div>
+					<div>{car.lastInspectionKind}</div>
+					<div>{car.lastInspectionResult}</div>
+				</div>
+			))}
+		</div>
+	);
 }
