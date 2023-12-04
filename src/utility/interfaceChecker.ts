be67636d2ab44cb8837478467a30b3f50bf56e09
YawHB <yawhboateng@gmail.com>
2023-12-04 11:57:54 +0100

    Added types to interfaceChecker

--- src/utility/interfaceChecker.ts
+++ src/utility/interfaceChecker.ts
@@ -1,18 +1,17 @@
+type TObject = IEmployee | ICustomer | IOrder | ICar;
 
-
-export function isEmployee(obj: any): obj is IEmployee {
-  	return typeof obj === 'object' && 'department' in obj;
+export function isEmployee(obj: TObject): obj is IEmployee {
+	return typeof obj === 'object' && 'department' in obj;
 }
 
-export function isCustomer(obj: any): obj is ICustomer {
-return typeof obj === 'object' && 'city' in obj;
+export function isCustomer(obj: TObject): obj is ICustomer {
+	return typeof obj === 'object' && 'city' in obj;
 }
 
-export function isOrder(obj: any): obj is IOrder {
-return typeof obj === 'object' && 'orderStartDate' in obj;
+export function isOrder(obj: TObject): obj is IOrder {
+	return typeof obj === 'object' && 'orderStartDate' in obj;
 }
 
-export function isCar(obj: any): obj is ICar {
-    return typeof obj === 'object' && 'brand' in obj;
+export function isCar(obj: TObject): obj is ICar {
+	return typeof obj === 'object' && 'brand' in obj;
 }
-
