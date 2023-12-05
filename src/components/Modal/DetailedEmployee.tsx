747c63219954f974edc28a119d45472386565c80
YawHB <yawhboateng@gmail.com>
2023-12-05 13:37:44 +0100

    Finished Employee modal view

--- src/components/Modal/DetailedEmployee.tsx
+++ src/components/Modal/DetailedEmployee.tsx
@@ -1,11 +1,50 @@
+// import { Card } from '@mantine/core';
+import { useEffect, useState } from 'react';
+import formatDate from '../../utility/dateFormat';
 
+interface DetailedEmployeeProps {
+	employee: IEmployee;
+}
 
+export default function DetailedEmployee({ employee }: DetailedEmployeeProps) {
+	const [employeeData, setEmployeeData] = useState<IEmployee | null>(null);
 
+	useEffect(() => {
+		async function getEmployee() {
+			//TODO Tilføj ENV fil til fetch kaldet
+			const response = await fetch(`http://localhost:3000/employees/${employee.id}`);
+			const data = await response.json();
+			setEmployeeData(data);
+		}
+		getEmployee();
+	}, [employee.id]);
+	console.log(employeeData);
 
-export default function DetailedEmployee({ employee }: { employee: IEmployee }) {
-    return (
-        <div>
-            {employee.firstName}
-        </div>
-    );
+	return (
+		<>
+			{employeeData && (
+				<div>
+					<div className='header-container'>{/* <h2>{carData?.car.registrationNumber}</h2> */}</div>
+					<div className='header-container'>
+						<h2>Bil Data</h2>
+					</div>
+					<section className='modal-container'>
+						<div className='modal-grid'>
+							<h3>Stilling</h3>
+							<div>{employeeData?.role}</div>
+
+							<h3>Afdeling</h3>
+							<div>{employeeData?.department}</div>
+							<h3>Medarbejder Navn</h3>
+							<div>
+								{employeeData?.firstName} {employeeData.lastName}
+							</div>
+							<h3>Seneste Inspektion</h3>
+							<div>{formatDate(new Date(employeeData.createdAt))}</div>
+						</div>
+					</section>
+				</div>
+			)}
+		</>
+	);
 }
