363a22a54dcbd2b05d951f711662347ccc195c6e
YawHB <yawhboateng@gmail.com>
2023-12-05 10:09:27 +0100

    Modal view completed (customer)

--- src/components/Modal/DetailedCustomer.tsx
+++ src/components/Modal/DetailedCustomer.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import formatDate from '../../utility/dateFormat';
 
 interface DetailedCustomerProps {
 	customer: ICustomer;
@@ -34,20 +35,46 @@ export default function DetailedCustomer({ customer }: DetailedCustomerProps) {
 			{customerData?.orders?.map((order) => (
 				<div key={order.id}>
 					{' '}
-					<div className='order-modal-container'>
-						<div className='grid-container'>
-							<div className='grid-item'>
-								<h3>Ordre id</h3>
-								<h3>Status</h3>
-								<h3>Oprettelsesdato</h3>
-								<h3>Seneste Opdatering</h3>
+					<section className='modal-container'>
+						<div className='modal-grid'>
+							<h3>Ordre id</h3>
+							<div>{order.id}</div>
+							<h3>Status</h3>
+							<div>{order.status}</div>
+							<h3>Oprettelses Dato</h3>
+							<div>{formatDate(new Date(order.createdAt))}</div>
+							<h3>Seneste Opdatering </h3>
+							<div>{formatDate(new Date(order.updatedAt))}</div>
+						</div>
+					</section>
+				</div>
+			))}
+			<div className='customer-modal-header-container'>
+				<h3>Bildata</h3>
+			</div>
+			{customerData?.cars.map((car) => (
+				<div key={car.id}>
+					<div className='modal-container'>
+						<div className='modal-grid'>
+							<h3>Model</h3>
+							<div>
+								{car.model} - {car.modelVariant}
 							</div>
-							<div className='grid-item'>
-								<div>{order.id}</div>
-								<div>{order.status}</div>
-								<div>{order.createdAt.toString()}</div>
-								<div>{order.updatedAt.toString()}</div>
-							</div>
+							<h3>Regnr.</h3>
+							<div>{car.registrationNumber}</div>
+							<h3>Synsdato</h3>
+
+							<div>{formatDate(new Date(car.lastInspectionDate))}</div>
+							<h3>Syns Type</h3>
+							<div>{car.lastInspectionKind}</div>
+							<h3>Syns Resultat</h3>
+							<div>{car.lastInspectionResult}</div>
+							<h3>Biloprettelse</h3>
+							<div>{formatDate(new Date(car.createdAt))}</div>
+							<h3>Kilometertal</h3>
+							<div>{car.mileage}</div>
+							<h3>Sidste registeret kilometertal </h3>
+							<div>{formatDate(new Date(car.updatedAt))}</div>
 						</div>
 					</div>
 				</div>
