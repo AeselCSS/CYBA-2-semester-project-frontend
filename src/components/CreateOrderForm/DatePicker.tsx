499e6377c82534d7a97ed94629459231442901c1
AliHMohammad <alihmohammad@hotmail.com>
2023-12-06 11:16:21 +0100

    Disabled weekends.

--- src/components/CreateOrderForm/DatePicker.tsx
+++ src/components/CreateOrderForm/DatePicker.tsx
@@ -12,16 +12,25 @@ interface Props {
 export default function DatePicker({unavailableDates, date, setDate}: Props) {
 	// 5 Days in the future
 	const minDate = new Date(Date.now() + 5 * 24 * 60 * 60 * 1000);
-	// 2 month in the future
-	const maxDate = new Date(Date.now() + 60 * 24 * 60 * 60 * 1000);
+	// 3 month in the future
+	const maxDate = new Date(Date.now() + 90 * 24 * 60 * 60 * 1000);
 
 	const disableTiles = ({ date }: { date: Date }): boolean => {
-		return unavailableDates.some((unavailableDate) => date.toDateString() === new Date(unavailableDate).toDateString())
+		// return unavailableDates.some((unavailableDate) => date.toDateString() === new Date(unavailableDate).toDateString())
+
+		if (unavailableDates.some((unavailableDate) => date.toDateString() === new Date(unavailableDate).toDateString())) {
+			return true;
+		} else if (date.getDay() === 0 || date.getDay() === 6) {
+			return true
+		}
+
+		return false;
 	}
 
 	return (
 		<Calendar
 			className="calender"
+			showNeighboringMonth={false}
 			value={date}
 			onChange={(value) => setDate(value)}
 			minDate={minDate}
