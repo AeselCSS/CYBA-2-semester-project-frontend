7a798b0583476df242508bd0a170af24ab38a267
YawHB <yawhboateng@gmail.com>
2023-12-07 11:10:37 +0100

    designed task divs

--- src/components/CreateOrderForm/TaskCheckbox.tsx
+++ src/components/CreateOrderForm/TaskCheckbox.tsx
@@ -1,19 +1,22 @@
 import calculatePrice from '../../utility/priceCalculator.ts';
+import classes from '../CreateOrderForm/CreateOrderForm.module.css';
 
 interface Props {
 	task: IAPITask;
 	register: any;
-
 }
 
 export default function TaskCheckbox({ task, register }: Props) {
-
 	return (
-		<div className="checkbox-div">
-			<input {...register("taskIds", {required: true, minLength: 1})} type='checkbox' value={String(task.id)}  />
-			<label htmlFor={String(task.id)}>{task.name}</label>
-			<p className="description">{task.description}</p>
-			<p className="price">{calculatePrice(task.time)},-</p>
+		<div className={classes.checkboxDiv}>
+			<div className={classes.inputAndDescrWrapper}>
+				<div className={classes.inputContainer}>
+					<input {...register('taskIds', { required: true, minLength: 1 })} type='checkbox' value={String(task.id)} />
+				</div>
+				<label htmlFor={String(task.id)}>{task.name}</label>
+			</div>
+			{/* <p className='description'>{task.description}</p> */}
+			<p className='price'>{calculatePrice(task.time)},-</p>
 		</div>
 	);
 }
