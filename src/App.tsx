6cb281efdd448486195ea37cae1465f9e82ab6d8
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-12-07 22:52:45 +0100

    moved routes to a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,16 @@
 import { UserProvider } from './context/UserProvider.tsx';
-import { Routes, Route } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
 import Toaster from './components/Toaster/Toaster.tsx';
 import '@mantine/core/styles.css';
+import { AppRoutes } from './routes/AppRoutes.tsx';
 
-// Pages
-import {
-	Homepage,
-	PageNotFound,
-	About,
-	Contact,
-	CreateProfile,
-	Redirect,
-	OrderOverview,
-	CarOverview,
-	CustomerProfile,
-	EmployeeOverview,
-	CustomerOverview,
-	CreateOrder,
-	CreateCar,
-	UpdateProfile,
-} from './pages';
 
 function App() {
 	return (
 		<UserProvider>
 			<MantineProvider>
 				<Toaster />
-				<Routes>
-					<Route path='/' element={<Homepage />} />
-					<Route path='/about' element={<About />} />
-					<Route path='/contact' element={<Contact />} />
-					<Route path='/redirect' element={<Redirect />} />
-
-					{/* Protected Routes */}
-					<Route path='/profile' element={<CustomerProfile />} />
-					<Route path='/profile/create' element={<CreateProfile />} />
-					<Route path='/profile/update' element={<UpdateProfile />} />
-					<Route path='/orders/create' element={<CreateOrder />} />
-					<Route path='/cars/create' element={<CreateCar />} />
-					<Route path='/employee/orders' element={<OrderOverview />} />
-					<Route path='/employee/cars' element={<CarOverview />} />
-					<Route path='/employee/employees' element={<EmployeeOverview />} />
-					<Route path='/employee/customers' element={<CustomerOverview />} />
-
-					{/* Catch-all Route */}
-					<Route path='*' element={<PageNotFound />} />
-				</Routes>
+				<AppRoutes />
 			</MantineProvider>
 		</UserProvider>
 	);
