beeabe0effc40cf9ed8512a652885f806eba097a
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-12-07 09:59:05 +0100

    removed customer props. user is now handled with UserContext

--- src/pages/CustomerProfile.tsx
+++ src/pages/CustomerProfile.tsx
@@ -1,39 +1,38 @@
+import { useContext, useEffect, useState } from 'react';
+import UserContext from '../context/userContext';
+import { userIsCustomer } from '../utility/userRoleChecker';
 import CarBox from '../components/CustomerProfile/CarBox';
 import CustomerBox from '../components/CustomerProfile/CustomerBox';
 import CustomerProfileGrid from '../components/CustomerProfile/CustomerProfileGrid';
-import PageLayout from './PageLayout';
-import { useEffect, useState } from 'react';
+import PageLayout from '../layouts/PageLayout/PageLayout';
 import OrdersBox from '../components/CustomerProfile/OrdersBox';
 import Loader from '../components/Loader/Loader';
+import { getSingleCustomer } from '../services/apiService';
 
-interface props {
-	customer: ICustomer;
-}
-
-export default function CustomerProfile({ customer }: props) {
+export default function CustomerProfile() {
 	const [customerData, setCustomerData] = useState<IAPISingleCustomer | null>(null);
-/*
-	const [cars, setCars] = useState<null | ICar[]>(null)
-*/
-	console.log("I am in CustomerProfile");
-	useEffect(() => {
-		async function getCustomer() {
-			//TODO Tilføj ENV fil til fetch kaldet
-			const response = await fetch(`http://localhost:3000/customers/${customer.id}`);
-			const data = await response.json();
-			setCustomerData(data);
+	const { user } = useContext(UserContext);
 
+	useEffect(() => {
+		if (userIsCustomer(user)) { // Check if the user is a customer
+			const customer = user as ICustomer;
+			getSingleCustomer(customer.id).then(setCustomerData);
 		}
-		getCustomer();
-	}, [customer.id]);
+
+	}, [user]);
+
+	if (!userIsCustomer(user)) {
+		// If the user is not a customer, deny access. We might want to redirect to a 404 page instead?
+		return <p>Access Denied</p>;
+	}
 
 	return (
 		<PageLayout>
 			{customerData ? (
 				<CustomerProfileGrid>
-					{<CustomerBox customerData={customerData} />}
-					{<CarBox customerData={customerData} />}
-					{<OrdersBox customerData={customerData} />}
+					<CustomerBox customerData={customerData} />
+					<CarBox customerData={customerData} />
+					<OrdersBox customerData={customerData} />
 				</CustomerProfileGrid>
 			) : (
 				<Loader />
