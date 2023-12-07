f5e7c2ff77a7c2613bbed77fa9ad153840cd572e
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-12-07 11:23:47 +0100

    refactored to use updated UserContext

--- src/modals/DetailedOrder/OrderAccordian.tsx
+++ src/modals/DetailedOrder/OrderAccordian.tsx
@@ -1,29 +1,32 @@
 import { useContext, useState } from 'react';
+import UserContext from '../../context/userContext';
+import { completeSubtaskInstance, initiateTaskInstance } from '../../services/apiService';
+import ChangeOrderStatusButton from '../../components/Buttons/ChangeOrderStatusButton';
+import { Status } from '../../utility/enums';
+import { userIsEmployee } from '../../utility/userRoleChecker';
 import { FaRegCircle, FaRegCircleCheck, FaRegCirclePlay } from 'react-icons/fa6';
-import userContext from '../../../context/userContext.ts';
-import { Role, Status } from '../../../enums.ts';
-import ChangeOrderStatusButton from '../../Buttons/ChangeOrderStatusButton.tsx';
-import { completeSubtaskInstance, initiateTaskInstance } from '../../../services/apiService.ts';
-import classes from './OrderAccordian.module.css'
+import styles from './OrderAccordian.module.css'
 
 interface AccordionProps {
 	order: ICurrentOrder;
-	setOrder: (newvalue: ICurrentOrder) => void;
+	setOrder: (newValue: ICurrentOrder) => void;
 }
 
 function Accordion({ order, setOrder }: AccordionProps) {
 	const [openTaskId, setOpenTaskId] = useState<number | null>(null);
-
-	const user = useContext(userContext);
+	const { user } = useContext(UserContext);
 
 	const toggleTask = (taskId: number) => {
 		setOpenTaskId(openTaskId === taskId ? null : taskId);
 	};
 
 	async function handleStartTask(taskId: number) {
-		const response = await initiateTaskInstance(taskId, (user as IEmployee).id);
-		console.log(response);
-		setOrder(response);
+		const isEmployee = userIsEmployee(user);
+		if (isEmployee) {
+			const employee = user as IEmployee;
+			const response = await initiateTaskInstance(taskId, employee.id);
+			setOrder(response);
+		}
 	}
 
 	async function handleCompleteSubtask(subtaskId: number) {
@@ -36,38 +39,35 @@ function Accordion({ order, setOrder }: AccordionProps) {
 		<>
 			{order.tasks && (
 				<div>
-					{order.tasks?.map((task) => (
+					{order.tasks.map((task) => (
 						<div key={task.id}>
-							<button className={classes.accordianLabel} onClick={() => toggleTask(task.id)}>
+							<button className={styles.accordianLabel} onClick={() => toggleTask(task.id)}>
 								{task.status === Status.IN_PROGRESS && <FaRegCirclePlay color='yellow' />}
 								{task.status === Status.COMPLETED && <FaRegCircleCheck color='green' />}
-								{task.name} ({task.subtasks.filter((subtask) => subtask.status === Status.COMPLETED).length}/
-								{task.subtasks.length})
+								{task.name} ({task.subtasks.filter((subtask) => subtask.status === Status.COMPLETED).length}/{task.subtasks.length})
 							</button>
 
-							{(user as IEmployee | ICustomer)?.role === Role.EMPLOYEE &&
-							task.status === Status.PENDING &&
-							(order.status === Status.PENDING || order.status === Status.IN_PROGRESS) ? (
-								<ChangeOrderStatusButton btnText='Start opgave' onClick={() => handleStartTask(task.id)} />
-							) : null}
+							{userIsEmployee(user) && task.status === Status.PENDING &&
+								(order.status === Status.PENDING || order.status === Status.IN_PROGRESS) && (
+									<ChangeOrderStatusButton btnText='Start opgave' onClick={() => handleStartTask(task.id)} />
+								)}
 
 							{openTaskId === task.id && (
-								<div className={classes.accordianContent}>
+								<div className={styles.accordianContent}>
 									{task.subtasks.map((subtask) => (
-										<div className={classes.contentItem} key={subtask.id}>
+										<div className={styles.contentItem} key={subtask.id}>
 											<div>
 												{subtask.status === Status.PENDING && <FaRegCircle />}
 												{subtask.status === Status.COMPLETED && <FaRegCircleCheck color='green' />}
 												{subtask.status === Status.IN_PROGRESS && <FaRegCirclePlay color='yellow' />}
 											</div>
 											{subtask.name} - {subtask.description}
-											{subtask.status === Status.IN_PROGRESS &&
-											(user as IEmployee | ICustomer)?.role === Role.EMPLOYEE ? (
+											{subtask.status === Status.IN_PROGRESS && userIsEmployee(user) && (
 												<ChangeOrderStatusButton
 													btnText='Færdig'
 													onClick={() => handleCompleteSubtask(subtask.id)}
 												/>
-											) : null}
+											)}
 										</div>
 									))}
 								</div>
