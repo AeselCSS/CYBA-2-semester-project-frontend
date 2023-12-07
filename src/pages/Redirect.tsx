6400df9950ed36749139f4e8d90384e4fd4832bd
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-12-07 11:22:47 +0100

    added logic to save userdata to localStorage

--- src/pages/Redirect.tsx
+++ src/pages/Redirect.tsx
@@ -1,57 +1,65 @@
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from 'react-router-dom';
+import UserContext from '../context/userContext';
 
-interface IRedirectProps {
-	setUser: (user: ICustomer | IEmployee | IAuthUser | null) => void;
-}
-
-export default function Redirect({ setUser }: IRedirectProps) {
-	const { user, isLoading, error } = useAuth0();
+export default function Redirect() {
+	const { user, isAuthenticated, isLoading, error } = useAuth0();
 	const navigate = useNavigate();
+	const { setUser } = useContext(UserContext);
 
-	console.log(user);
+	const saveUserToLocaleStorage = (userData: UserUnion) => {
+		localStorage.setItem('userData', JSON.stringify(userData));
+	};
 
 	useEffect(() => {
 		async function getUser() {
-			// if (!user) throw new Error('Auth0 user not defined');
+			if (!isAuthenticated || !user) {
+				// Handle the case when the user is not authenticated
+				return;
+			}
 
-			// if (!isAuthenticated) {
-			// 	return;
-			// }
-			if (user) {
-				const res = await fetch(`http://localhost:3000/${user!.cybaRoles}s/${user!.sub}`);
+			try {
+				const res = await fetch(`http://localhost:3000/${user.cybaRoles}s/${user.sub}`);
 
 				if (res.ok) {
-					//User eksisterer
-					console.log(res);
-
 					const data = await res.json();
 
 					if (user.cybaRoles[0] === 'employee') {
-						console.log('I AM EMPLOYEE');
-
+						saveUserToLocaleStorage(data as IEmployee)
 						setUser(data as IEmployee);
 						navigate('/employee/orders');
-					} else {
+					}
+
+					if (user.cybaRoles[0] === 'customer') {
+						saveUserToLocaleStorage(data.customer as ICustomer)
 						setUser(data.customer as ICustomer);
-						console.log("REDIRECTING TO PROFILE");
 						navigate('/profile');
 					}
-				} else {
-					console.log(res);
-					console.log(user);
 
+					if (!user.cybaRoles) {
 					setUser(user as IAuthUser);
-
 					navigate('/profile/create');
-					//User eksisterer ikke
+					}
 				}
+			} catch (error) {
+				console.error('Error fetching user data:', error);
+				// Handle the error appropriately
 			}
 		}
 
-		getUser();
-	}, [user, setUser, navigate]);
+		if (!isLoading) {
+			getUser();
+		}
+	}, [user, isAuthenticated, isLoading, setUser, navigate]);
 
-	return <>{isLoading && !error && <p>Loading...</p>}</>;
+	if (isLoading) {
+		return <p>Loading...</p>;
+	}
+
+	if (error) {
+		return <p>Error: {error.message}</p>;
+	}
+
+	return null;
 }
