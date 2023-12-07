a030d1a2f018faf95c11796665c87f006b5f064b
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-12-07 20:20:05 +0100

    changed returnTo path

--- src/components/Buttons/SignupButton.tsx
+++ src/components/Buttons/SignupButton.tsx
@@ -7,7 +7,7 @@ export const SignupButton: React.FC = () => {
 	const handleSignUp = async () => {
 		await loginWithRedirect({
 			appState: {
-				returnTo: '/profile',
+				returnTo: '/redirect',
 			},
 			authorizationParams: {
 				prompt: 'login',
