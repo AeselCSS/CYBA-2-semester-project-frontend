9a6ddb07b6200acefbf5908e4e26c0f3a2e05faf
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-12-07 09:47:39 +0100

    changed imports, UserProvider, and removed logs

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,54 @@
-import Homepage from './pages/Homepage';
+import { UserProvider } from './context/UserProvider.tsx';
 import { Routes, Route } from 'react-router-dom';
-import PageNotFound from './pages/PageNotFound';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import CreateProfile from './pages/CreateProfile';
-import { useState } from 'react';
-import Redirect from './pages/Redirect';
-import OrderOverview from './pages/OrderOverview.tsx';
-import CarOverview from './pages/CarOverview.tsx';
-import CustomerProfile from './pages/CustomerProfile.tsx';
-import EmployeeOverview from './pages/EmployeeOverview.tsx';
-import CustomerOverview from './pages/CustomerOverview.tsx';
-import CreateOrder from './pages/CreateOrder.tsx';
-import CreateCar from './pages/CreateCar.tsx';
-import userContext from './context/userContext.ts';
-
+import { MantineProvider } from '@mantine/core';
+import Toaster from './components/Toaster/Toaster.tsx';
 import '@mantine/core/styles.css';
 
-import { MantineProvider } from '@mantine/core';
-import UpdateProfile from './pages/UpdateProfile.tsx';
-import Toaster from './components/Toaster/Toaster.tsx';
+// Pages
+import {
+	Homepage,
+	PageNotFound,
+	About,
+	Contact,
+	// CreateProfile,
+	Redirect,
+	// OrderOverview,
+	// CarOverview,
+	CustomerProfile,
+	// EmployeeOverview,
+	// CustomerOverview,
+	// CreateOrder,
+	// CreateCar,
+	// UpdateProfile,
+} from './pages';
 
 function App() {
-	const [user, setUser] = useState<ICustomer | IEmployee | IAuthUser | null>(null);
-
-	console.log(user);
-	console.log('I am in redirect');
-
 	return (
-
-		<userContext.Provider value={user}>
+		<UserProvider>
 			<MantineProvider>
-				<Toaster/>
+				<Toaster />
 				<Routes>
 					<Route path='/' element={<Homepage />} />
 					<Route path='/about' element={<About />} />
 					<Route path='/contact' element={<Contact />} />
-					<Route path='/orders/create' element={user && <CreateOrder customer={user as ICustomer} />} />
-					<Route path='/employee/orders' element={user && <OrderOverview employee={user as IEmployee} />} />
-					<Route path='/employee/cars' element={user && <CarOverview employee={user as IEmployee} />} />
-					<Route path='/employee/employees' element={user && <EmployeeOverview employee={user as IEmployee} />} />
-					<Route path='/employee/customers' element={user && <CustomerOverview employee={user as IEmployee} />} />
-					<Route path='/profile' element={user && <CustomerProfile customer={user as ICustomer} />} />
-					<Route path='/profile/create' element={user && <CreateProfile authUser={user as IAuthUser} />} />
-					<Route path='/profile/update' element={user && <UpdateProfile customer={user as ICustomer} />} />
-					<Route path='/cars/create' element={user && <CreateCar customer={user as ICustomer} />} />
-					<Route path='/redirect' element={<Redirect setUser={setUser} />} />
+					<Route path='/redirect' element={<Redirect />} />
+
+					{/* Protected Routes */}
+					<Route path='/profile' element={<CustomerProfile />} />
+					{/*<Route path='/profile/create' element={<CreateProfile />} />*/}
+					{/*<Route path='/profile/update' element={<UpdateProfile />} />*/}
+					{/*<Route path='/orders/create' element={<CreateOrder />} />*/}
+					{/*<Route path='/cars/create' element={<CreateCar />} />*/}
+					{/*<Route path='/employee/orders' element={<OrderOverview />} />*/}
+					{/*<Route path='/employee/cars' element={<CarOverview />} />*/}
+					{/*<Route path='/employee/employees' element={<EmployeeOverview />} />*/}
+					{/*<Route path='/employee/customers' element={<CustomerOverview />} />*/}
+
+					{/* Catch-all Route */}
 					<Route path='*' element={<PageNotFound />} />
 				</Routes>
 			</MantineProvider>
-		</userContext.Provider>
-
+		</UserProvider>
 	);
 }
 
