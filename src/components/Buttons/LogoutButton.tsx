b2947c06a3c96647430b0ca29be3c6666438841d
Chris Skov Sørensen <chso0002@stud.kea.dk>
2023-12-07 11:24:38 +0100

    now removing userData from localStorage upon logout

--- src/components/Buttons/LogoutButton.tsx
+++ src/components/Buttons/LogoutButton.tsx
@@ -1,13 +1,13 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import React from 'react';
 
-
-
-
 export const LogoutButton: React.FC = () => {
 	const { logout } = useAuth0();
 
 	const handleLogout = () => {
+			// remove userData from localStorage
+			localStorage.removeItem('userData');
+
 		logout({
 			logoutParams: {
 				returnTo: window.location.origin,
